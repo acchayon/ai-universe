070e16692447c8a5b18459f4876a3210434819ef
acchayone <adhikaryac81@gmail.comm>
2023-03-02 22:15:51 +0600

    successfully load data in console from api

--- js/universe.js
+++ js/universe.js
@@ -1,7 +1,11 @@
 const loadData = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayData(data))
+}
+
+const displayData = allData => {
+    console.log(allData);
 }
 
 loadData()
