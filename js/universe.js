fa04ff6224675b8ab8e2a40852f76eaaeef2fd6b
acchayone <adhikaryac81@gmail.comm>
2023-03-03 21:04:13 +0600

    added Modal

--- js/universe.js
+++ js/universe.js
@@ -1,16 +1,18 @@
 const loadData = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => displayData(data.data))
+    .then(data => displayData(data.data.tools.slice(0,6)))
     
 }
 
 const displayData = allData => {
     // console.log(allData);
-    toggleLoader(true)
+    toggleLoader(true);
+
+    
     const cardContainer = document.getElementById('card-container');
-    // allData = allData.slice(0,3)
-    allData.tools.forEach(data => {
+    
+    allData.forEach(data => {
         console.log(data)
         const makeDiv = document.createElement('div');
         makeDiv.classList.add('col');
@@ -20,17 +22,27 @@ const displayData = allData => {
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
                 <p class="card-text">
+                    1.${data.features[0]} <br>
+                    2.${data.features[1]} <br>
+                    3.${data.features[2]}
                 </p>
             </div>
             <hr>
             <div class="d-flex justify-content-between">
             <h4 class="card-title px-3">${data.name}</h4>
-            <button type="button" class="btn">&rightarrow;</button>
+            <button type="button" class="btn btn-outline-success m-3" data-bs-toggle="modal" data-bs-target="#exampleModal">&rightarrow;</button>
             </div>
         </div>
         `;
+
+        
         cardContainer.appendChild(makeDiv); 
+
+        
     })
+
+    
+
     toggleLoader(false)
     
 }
