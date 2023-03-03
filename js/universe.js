fc2379ecd4d5879b40d7286f5d16932c0708a18c
acchayone <adhikaryac81@gmail.comm>
2023-03-03 15:57:16 +0600

    added spinner for loading data

--- js/universe.js
+++ js/universe.js
@@ -2,11 +2,14 @@ const loadData = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
     .then(data => displayData(data.data))
+    
 }
 
 const displayData = allData => {
     // console.log(allData);
+    toggleLoader(true)
     const cardContainer = document.getElementById('card-container');
+    // allData = allData.slice(0,3)
     allData.tools.forEach(data => {
         console.log(data)
         const makeDiv = document.createElement('div');
@@ -16,17 +19,31 @@ const displayData = allData => {
             <img src="${data.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                <p class="card-text">1.${data.features[0]} <br> 2.${data.features[1]} <br> 3.${data.features[2]}</p>
+                <p class="card-text">
+                </p>
             </div>
             <hr>
             <div class="d-flex justify-content-between">
             <h4 class="card-title px-3">${data.name}</h4>
-            <button type="button" class="btn ">&rightarrow;</button>
+            <button type="button" class="btn">&rightarrow;</button>
             </div>
         </div>
         `;
         cardContainer.appendChild(makeDiv); 
     })
+    toggleLoader(false)
+    
+}
+
+
+const toggleLoader = isLoading => {
+    const spinLoader = document.getElementById('loader');
+    if(isLoading === 0){
+        spinLoader.classList.remove('d-none');
+    }
+    else{
+        spinLoader.classList.add('d-none');
+    }
 }
 
 loadData()
